d119b3c2337c84c795b73e39088d44e93432d2ab
bridaybrummer <brian@tommybrummer.co.za>
2024-05-24 15:45:53 +0200

    updated functions

--- R/bind_linelist.R
+++ R/bind_linelist.R
@@ -1,6 +1,11 @@
 #' bind_linelist
 #'
-#' @param directory
+#' @param directory a string character denoting the directory where the linelist is stored
+#'  This function will find all .xlsx files stored in a directory or folder. Note that the directory is the name of the folder where the.xlsx files are stored and NOT the name of the .xlsx file itself.
+#'  The function converte the read in .xlsx file all to character to ensure all data can be bound together.
+#'  type.convert is then implemented for appropriate type conversion however it is not perfect and may need to be adjusted.
+#'
+#'  We recommend that the output is then fed into stata2script()
 #'
 #' @return a tibble
 #' @export
@@ -8,15 +13,22 @@
 #' @import readxl
 #'
 #' @examples
-#' not much
-bind_linelist<- function( directory) {
-  # List all Excel files in the directory
+#' bind_linelist("database/linelist")
+bind_linelist <- function(directory) {
   excel_files <- list.files(directory, pattern = "\\.xlsx$", full.names = TRUE)
 
-  excel_files
-  all_data<- NULL
-  # Read and bind Excel files into a single data frame
-  all_data <- lapply(excel_files, read_excel) %>%
-    bind_rows()
+  # Read all Excel files and handle type inconsistencies
+  df_list <- lapply(excel_files, function(file) {
+    df <- read_excel(file)
+    df <- df %>%
+      mutate(across(everything(), as.character))# %>%
+    #mutate(across(where(is.numeric), as.character)) %>%
+    #mutate(across(where(is.factor), as.character))
+    return(df)
+  })
+
+  # Bind all data frames into one
+  all_data <- bind_rows(df_list)%>%type.convert()
+
   return(all_data)
 }
---
+++ R/mutate_dates.R
@@ -0,0 +1,53 @@
+#' Mutate_date
+#'
+#' The function gives mutates individual date related variables that are compatible withother function in this package
+#' This will allow you to left_join datasets with date_templates for graphic with zero reporting.
+#' If converting dates from excel, use openxlsx::convertToDateTime(data$ExcelNumber, origin = "1900-01-01")
+#'
+#'
+#' @param data a tibble or data.frame
+#' @param date_index the date variable. This should be in an lubridate::as_date() format. ( "YYYY-MM_DD")
+#'
+#' @return a tibble with the follwing variabels as factors: date, day, epiweek (CDC format), month, year
+#' @export
+#'
+#' @examples
+#' mutate_dates(data, date_index = "notification_date")
+#' # to see whether it is creating dates propoperly
+#'
+#' dates<- seq(as.Date("2020-01-01"), as.Date("2020-12-31"), by = "day")
+#'
+#' tibble(
+#'  date = dates)%>%
+#'  mutate_dates(., "date")%>%
+#'  group_by(month) %>%
+#'  summarise(day_in_month = n())
+#'
+#'  # or you could see the first and last day of the epiweek, which, according to CDC is, Sunday
+#'
+#' tibble(
+#'  date = dates)%>%
+#'  mutate_dates(., "date")%>%
+#'  group_by(epiweek) %>%
+#'  filter( day == min(day ) | day == max(day))
+#'
+#'
+mutate_dates<- function(data, date_index){
+
+  #run functions to get date in correct format, try with year first or day first, try with excel and stats date formats
+  # TO DO
+  ## could try make an argument that give a pre_fix or suffix to the date variables for instance, year_notification and so on
+  data <- data %>%
+    mutate(date = as_date(data[[date_index]]))%>%
+    mutate(
+      day_name = as.factor( format(date, "%A") ),
+      day = as.factor(as.integer(format(date, "%d"))),
+      epiweek =  date %>%as_epiweek%>%as.character()%>%gsub("\\d+-W", "", .)%>%as.integer(),
+      month = factor(as_yearmonth(date)%>%as.character()%>%gsub("\\d+-", "", .), levels = month.abb),
+      year = epiyear(date)
+    )%>%
+      mutate(across(all_of(standard_group_vars), as.factor))
+
+  return(data)
+
+}
