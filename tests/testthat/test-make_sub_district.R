11593053d7981b7097d9964b9f70f4a1836556f4
bridaybrummer <brian@tommybrummer.co.za>
2025-08-18 16:13:38 +0200

    remove one test that calls an RDS object

--- tests/testthat/test-make_sub_district.R
+++ tests/testthat/test-make_sub_district.R
@@ -1,19 +1,19 @@
 library(testthat)
 library(tibble)
 
-test_that("Make_sub_district_name maps exact normalized keys and keeps unmatched", {
+#test_that("Make_sub_district_name maps exact normalized keys and keeps unmatched", {
 
-    readr::read_rds("inst/extdata/sa_subdistrict_dictionary.rds")-> dict_long
+#    readr::read_rds("inst/extdata/sa_subdistrict_dictionary.rds")-> dict_long
 
-    dict  <- dict_long %>%
-        dplyr::group_by(province, district, subdistrict_code, standard_subdistrict) %>%
-        dplyr::summarise(subdistrict_variant = list(unique(variant)), .groups = "drop")
+#    dict  <- dict_long %>%
+#        dplyr::group_by(province, district, subdistrict_code, standard_subdistrict) %>%
+#        dplyr::summarise(subdistrict_variant = list(unique(variant)), .groups = "drop")
 
-  df <- tibble::tibble(subdistrict = c("dr pixley ka seme", "dr beyers naudé", "city of cape Town", "port saint johns"))
-  res <- mutate_sub_district(df, subdistrict_variable = "subdistrict", dict = dict, use_fuzzy = FALSE, unmatched = "keep_raw")
+#  df <- tibble::tibble(subdistrict = c("dr pixley ka seme", "dr beyers naudé", "city of cape Town", "port saint johns"))
+#  res <- mutate_sub_district(df, subdistrict_variable = "subdistrict", dict = dict, use_fuzzy = FALSE, unmatched = "keep_raw")
 
-  expect_equal(res$subdistrict_standard, c("Pixley ka Seme", "Dr Beyers Naudé", "City of Cape Town", "Port St Johns"))
-})
+#  expect_equal(res$subdistrict_standard, c("Pixley ka Seme", "Dr Beyers Naudé", "City of Cape Town", "Port St Johns"))
+#})
 
 
 test_that("unmatched = 'NA' sets unmatched values to NA", {
